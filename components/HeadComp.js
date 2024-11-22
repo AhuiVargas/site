fde185b15121d57ffbfa70d3646b57e8ee828862
Ahui <ahuijr@gmail.com>
2024-11-22 14:22:47 -0500

    fix build version warns

--- components/HeadComp.js
+++ components/HeadComp.js
@@ -9,14 +9,14 @@ const HeadComp = () => {
       
       <meta property="og:title" content="ahuizotl.dev" />
       <meta property="og:description" content="Web Development" />
-      <meta property="og:image" itemprop="image" content="https://ahuizotl.dev/og-image.png" />
+      <meta property="og:image" itemProp="image" content="https://ahuizotl.dev/og-image.png" />
       <meta property="og:url" content="https://ahuizotl.dev" />
       <meta property="og:type" content="website" />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
-      <meta itemprop="url" content="https://ahuizotl.dev" />
-      <meta itemprop="thumbnailUrl" content="https://ahuizotl.dev/og-image.png" />
-      <meta itemprop="image" content="https://ahuizotl.dev/og-image.png" />
+      <meta itemProp="url" content="https://ahuizotl.dev" />
+      <meta itemProp="thumbnailUrl" content="https://ahuizotl.dev/og-image.png" />
+      <meta itemProp="image" content="https://ahuizotl.dev/og-image.png" />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content="ahuizotl.dev" />
--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,16 @@
-import App from "next/app";
 import React from "react";
-import Providers from './providers';
+import Providers from "./providers";
 import { GlobalStyle } from "../styles";
 
-class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-    pageProps.query = ctx.query;
-    return { pageProps };
-  }
-
-  render() {
-    const { Component, pageProps } = this.props;
-
-    return (
-      <Providers>
-        <>
-          <GlobalStyle />
-          <Component {...pageProps} />
-        </>
-      </Providers>
-    );
-  }
+function MyApp({ Component, pageProps }) {
+  return (
+    <Providers>
+      <>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </>
+    </Providers>
+  );
 }
 
 export default MyApp;
