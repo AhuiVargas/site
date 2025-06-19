ae666982fe1f0119a29daaa9ee1747b7827419ee
Ahui <ahuijr@gmail.com>
2025-06-19 00:32:13 -0500

    added analytics scripts, cleaned up document structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,40 @@
+// pages/_app.js
 import React from "react";
+import Script from "next/script";
 import Providers from "./providers";
 import { GlobalStyle } from "../styles";
+import HeadComp from "../components/HeadComp";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Providers>
-      <>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </>
-    </Providers>
-  );
+	return (
+		<>
+			<HeadComp />
+
+			<Script
+				src="https://scripts.simpleanalyticscdn.com/latest.js"
+				strategy="afterInteractive"
+			/>
+
+			<Script
+				id="clarity-script"
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+				    __html: `
+                        (function(c,l,a,r,i,t,y){
+                          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                        })(window, document, "clarity", "script", "s1s1d8afat");
+                    `,
+				}}
+			/>
+
+			<Providers>
+				<GlobalStyle />
+				<Component {...pageProps} />
+			</Providers>
+		</>
+	);
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,12 @@ import gsap from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
 import { useRouter } from "next/router";
 import {
-  Nav,
-  Social,
-  About,
-  Section,
-  SectionMobile,
-  WorkCard,
-  HeadComp,
+	Nav,
+	Social,
+	About,
+	Section,
+	SectionMobile,
+	WorkCard,
 } from "../components";
 
 import en from "../locales/en";
@@ -19,169 +18,164 @@ import es from "../locales/es";
 gsap.registerPlugin(ScrollTrigger);
 
 const Cursor = styled.div`
-  position: fixed;
-  width: 37.5rem;
-  height: 37.5rem;
-  background: url(/pointer.png) no-repeat center center;
-  background-size: cover;
-  mix-blend-mode: difference;
-  transform: translate(-50%, -50%) rotate(180deg);
+	position: fixed;
+	width: 37.5rem;
+	height: 37.5rem;
+	background: url(/pointer.png) no-repeat center center;
+	background-size: cover;
+	mix-blend-mode: difference;
+	transform: translate(-50%, -50%) rotate(180deg);
 `;
 
 const HomeContainer = styled.div`
-  overflow: hidden;
-  display: block;
-  position: relative;
+	overflow: hidden;
+	display: block;
+	position: relative;
 `;
 
 const Home = () => {
-  const sectionRefs = useRef([]);
-  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
-  const [isMobile, setIsMobile] = useState(false);
+	const sectionRefs = useRef([]);
+	const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+	const [isMobile, setIsMobile] = useState(false);
 
-  const { locale } = useRouter();
-  const t = locale === "en" ? en : es;
+	const { locale } = useRouter();
+	const t = locale === "en" ? en : es;
 
-  useEffect(() => {
-    const handleMouseMove = (event) => {
-      setCursorPosition({
-        x: event.clientX,
-        y: event.clientY,
-      });
-    };
+	useEffect(() => {
+		const handleMouseMove = (event) => {
+			setCursorPosition({
+				x: event.clientX,
+				y: event.clientY,
+			});
+		};
 
-    window.addEventListener("mousemove", handleMouseMove);
-    return () => window.removeEventListener("mousemove", handleMouseMove);
-  }, []);
+		window.addEventListener("mousemove", handleMouseMove);
+		return () => window.removeEventListener("mousemove", handleMouseMove);
+	}, []);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
+	useEffect(() => {
+		const handleResize = () => {
+			setIsMobile(window.innerWidth <= 768);
+		};
 
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+		handleResize();
+		window.addEventListener("resize", handleResize);
+		return () => window.removeEventListener("resize", handleResize);
+	}, []);
 
-  useEffect(() => {
-    gsap.killTweensOf(sectionRefs.current);
+	useEffect(() => {
+		gsap.killTweensOf(sectionRefs.current);
 
-    sectionRefs.current.forEach((section) => {
-      gsap.fromTo(
-        section,
-        { opacity: 0, y: 100, scale: 0.9 },
-        {
-          opacity: 1,
-          y: 0,
-          scale: 1,
-          scrollTrigger: {
-            trigger: section,
-            start: "center 100%",
-            end: "center 10%",
-            scrub: 1,
-            markers: false,
-          },
-          ease: "power3.out",
-        }
-      );
-    });
-  }, []);
+		sectionRefs.current.forEach((section) => {
+			gsap.fromTo(
+				section,
+				{ opacity: 0, y: 100, scale: 0.9 },
+				{
+					opacity: 1,
+					y: 0,
+					scale: 1,
+					scrollTrigger: {
+						trigger: section,
+						start: "center 100%",
+						end: "center 10%",
+						scrub: 1,
+						markers: false,
+					},
+					ease: "power3.out",
+				}
+			);
+		});
+	}, []);
 
-  return (
-    <>
-      <HeadComp />
-      <main>
-        <HomeContainer>
-          <Cursor
-            style={{
-              top: `${cursorPosition.y}px`,
-              left: `${cursorPosition.x}px`,
-            }}
-          />
-          <Nav />
-          <About />
-          <div ref={(el) => (sectionRefs.current[0] = el)}>
-            <WorkCard />
-          </div>
+	return (
+		<HomeContainer>
+			<Cursor
+				style={{
+					top: `${cursorPosition.y}px`,
+					left: `${cursorPosition.x}px`,
+				}}
+			/>
+			<Nav />
+			<About />
+			<div ref={(el) => (sectionRefs.current[0] = el)}>
+				<WorkCard />
+			</div>
 
-          <div ref={(el) => (sectionRefs.current[1] = el)}>
-            {isMobile ? (
-              <SectionMobile
-                titleImageSrc="/Rappi.png"
-                subtitle="Unicorn startup fintech/delivery"
-                description="Tech stack: Angular, RXJS, NGRX, Typescript, GraphQL, Material Components, Single SPA, HTML, CSS"
-                ctaText={t.cta}
-                ctaLink="https://www.rappipay.com/"
-                imageSrc="/RappiSS.png"
-                backgroundImage="/RappiBG.png"
-              />
-            ) : (
-              <Section
-                titleImageSrc="/Rappi.png"
-                subtitle="Unicorn startup fintech/delivery"
-                description="Tech stack: Angular, RXJS, NGRX, Typescript, GraphQL, Material Components, Single SPA, HTML, CSS"
-                ctaText={t.cta}
-                ctaLink="https://www.rappipay.com/"
-                imageSrc="/RappiSS.png"
-                backgroundImage="/RappiBG.png"
-              />
-            )}
-          </div>
+			<div ref={(el) => (sectionRefs.current[1] = el)}>
+				{isMobile ? (
+					<SectionMobile
+						titleImageSrc="/Rappi.png"
+						subtitle="Unicorn startup fintech/delivery"
+						description="Tech stack: Angular, RXJS, NGRX, Typescript, GraphQL, Material Components, Single SPA, HTML, CSS"
+						ctaText={t.cta}
+						ctaLink="https://www.rappipay.com/"
+						imageSrc="/RappiSS.png"
+						backgroundImage="/RappiBG.png"
+					/>
+				) : (
+					<Section
+						titleImageSrc="/Rappi.png"
+						subtitle="Unicorn startup fintech/delivery"
+						description="Tech stack: Angular, RXJS, NGRX, Typescript, GraphQL, Material Components, Single SPA, HTML, CSS"
+						ctaText={t.cta}
+						ctaLink="https://www.rappipay.com/"
+						imageSrc="/RappiSS.png"
+						backgroundImage="/RappiBG.png"
+					/>
+				)}
+			</div>
 
-          <div ref={(el) => (sectionRefs.current[2] = el)}>
-            {isMobile ? (
-              <SectionMobile
-                titleImageSrc="/Kapital.svg"
-                subtitle="Fintech, AI, banking and internal tooling"
-                description="Tech stack: NextJS, Typescript, Redux, React, Tailwind, Git, Bitbucket, REST"
-                ctaText={t.cta}
-                ctaLink="https://www.kapital.cc/productos/kapital-ai"
-                imageSrc="/KapitalSS.png"
-                backgroundImage="/KapitalBG.png"
-              />
-            ) : (
-              <Section
-                titleImageSrc="/Kapital.svg"
-                subtitle="Fintech, AI, banking and internal tooling"
-                description="Tech stack: NextJS, Typescript, Redux, React, Tailwind, Git, Bitbucket, REST"
-                ctaText={t.cta}
-                ctaLink="https://www.kapital.cc/productos/kapital-ai"
-                imageSrc="/KapitalSS.png"
-                backgroundImage="/KapitalBG.png"
-              />
-            )}
-          </div>
+			<div ref={(el) => (sectionRefs.current[2] = el)}>
+				{isMobile ? (
+					<SectionMobile
+						titleImageSrc="/Kapital.svg"
+						subtitle="Fintech, AI, banking and internal tooling"
+						description="Tech stack: NextJS, Typescript, Redux, React, Tailwind, Git, Bitbucket, REST"
+						ctaText={t.cta}
+						ctaLink="https://www.kapital.cc/productos/kapital-ai"
+						imageSrc="/KapitalSS.png"
+						backgroundImage="/KapitalBG.png"
+					/>
+				) : (
+					<Section
+						titleImageSrc="/Kapital.svg"
+						subtitle="Fintech, AI, banking and internal tooling"
+						description="Tech stack: NextJS, Typescript, Redux, React, Tailwind, Git, Bitbucket, REST"
+						ctaText={t.cta}
+						ctaLink="https://www.kapital.cc/productos/kapital-ai"
+						imageSrc="/KapitalSS.png"
+						backgroundImage="/KapitalBG.png"
+					/>
+				)}
+			</div>
 
-          <div ref={(el) => (sectionRefs.current[3] = el)}>
-            {isMobile ? (
-              <SectionMobile
-                titleImageSrc="/KingTide.svg"
-                subtitle="KT Venture capital studio"
-                description="Tech stack: React, NextJS, Styled Components, GraphQL, REST, HTML, CSS."
-                ctaText={t.cta}
-                ctaLink="https://www.kingtide.com/"
-                imageSrc="/KingTideSS.png"
-                backgroundImage="/KingTideBG.png"
-              />
-            ) : (
-              <Section
-                titleImageSrc="/KingTide.svg"
-                subtitle="KT Venture capital studio"
-                description="Tech stack: React, NextJS, Styled Components, GraphQL, REST, HTML, CSS."
-                ctaText={t.cta}
-                ctaLink="https://www.kingtide.com/"
-                imageSrc="/KingTideSS.png"
-                backgroundImage="/KingTideBG.png"
-              />
-            )}
-          </div>
+			<div ref={(el) => (sectionRefs.current[3] = el)}>
+				{isMobile ? (
+					<SectionMobile
+						titleImageSrc="/KingTide.svg"
+						subtitle="KT Venture capital studio"
+						description="Tech stack: React, NextJS, Styled Components, GraphQL, REST, HTML, CSS."
+						ctaText={t.cta}
+						ctaLink="https://www.kingtide.com/"
+						imageSrc="/KingTideSS.png"
+						backgroundImage="/KingTideBG.png"
+					/>
+				) : (
+					<Section
+						titleImageSrc="/KingTide.svg"
+						subtitle="KT Venture capital studio"
+						description="Tech stack: React, NextJS, Styled Components, GraphQL, REST, HTML, CSS."
+						ctaText={t.cta}
+						ctaLink="https://www.kingtide.com/"
+						imageSrc="/KingTideSS.png"
+						backgroundImage="/KingTideBG.png"
+					/>
+				)}
+			</div>
 
-          <Social />
-        </HomeContainer>
-      </main>
-    </>
-  );
+			<Social />
+		</HomeContainer>
+	);
 };
 
 export default Home;
